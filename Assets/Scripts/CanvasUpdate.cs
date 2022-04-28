4609c7c478e1a08379e9fbfcdcc839c4f6cd9a85
CharlesDefoy1919 <c.defoy@student.isartdigital.com>
2022-04-28 16:56:24 -0400

    Canvas

---
+++ Assets/Scripts/CanvasUpdate.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasUpdate : MonoBehaviour
+{
+    public GameManager GameManagerScript;
+    public GameObject MyGameManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MyGameManager = GameObject.FindGameObjectWithTag("GameManager");
+
+        GameManagerScript = MyGameManager.GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
