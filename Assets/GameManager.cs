b18fbdac55cb1644c7884644a2dbcee772f76785
CharlesDefoy1919 <c.defoy@student.isartdigital.com>
2022-04-29 10:10:22 -0400

    Game End

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
@@ -12,13 +13,24 @@ public class GameManager : MonoBehaviour
 
     public AudioClip deathSound;
 
+    public GameObject EndGameCam;
+    public GameObject EndMessage;
+    public GameObject GameHUD;
+    public TextMeshProUGUI EndGameWinMessage;
+    public Camera CamDepth;
+
+
     // Start is called before the first frame update
     void Start()
     {
         PlayerHealthTeam1Pts = StartingPlayerHealthPts;
         PlayerHealthTeam2Pts = StartingPlayerHealthPts;
 
-        
+        EndGameWinMessage = EndMessage.GetComponentInChildren<TextMeshProUGUI>();
+
+        CamDepth = EndGameCam.GetComponent<Camera>();
+        CamDepth.depth = -1;
+
     }
 
     // Update is called once per frame
@@ -26,15 +38,31 @@ public class GameManager : MonoBehaviour
     {
         if (PlayerHealthTeam1Pts <= 0)
         {
-            Debug.Log("Team 2 wins");
+            //Debug.Log("Team 2 wins");
             //DeathSoud = true;
 
+            //EndGameCam.SetActive(true);
+            EndMessage.SetActive(true);
+            GameHUD.SetActive(false);
+
+            CamDepth.depth = 1;
+
+            EndGameWinMessage.text = ("BLUE TEAM WINS");
+            EndGameWinMessage.color = Color.blue;
 
         }
 
         if (PlayerHealthTeam2Pts <= 0)
         {
-            Debug.Log("Team 1 wins");
+            //Debug.Log("Team 1 wins");
+           // EndGameCam.SetActive(true);
+            EndMessage.SetActive(true);
+            GameHUD.SetActive(false);
+
+            CamDepth.depth = 1;
+
+            EndGameWinMessage.text = ("RED TEAM WINS");
+            EndGameWinMessage.color = Color.red;
             //DeathSoud = true;
         }
 
